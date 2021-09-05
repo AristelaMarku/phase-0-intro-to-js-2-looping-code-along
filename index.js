1c4916fcc20c1ba77c06de364f5c788a4bc35f51
Aristela Marku <aristela.marku@gmail.com>
2021-09-05 22:09:10 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const arrNames = ["stela", "arbi", "mami"];
+function writeCards(arrNames,name){
+const newArray=[];
+for(let i=0;i<arrNames.length;i++){
+    newArray[i]=`Thank you, ${arrNames[i]}, for the wonderful ${name} gift!`
+}
+return newArray;
+}
+
+function countDown(num){
+    let i=num;
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+
+}
